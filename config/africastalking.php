65258cc8270b42b63e455364628e9a316a8d74eb
nyasingajeff <nyasingajeff@gmail.com>
2020-10-12 17:18:07 +0300

    x

--- config/africastalking.php
+++ config/africastalking.php
@@ -1,5 +1,5 @@
 <?php
 return [
-    'username_sandbox' => env('AT_SANDBOX_USERNAME', "sandbox"),
-    'api_key_sandbox' => env('AT_SANDBOX_API_KEY', "NOAPIKEY")
+    'username_sandbox' => env('nyasinga', "sandbox"),
+    'api_key_sandbox' => env('a8d79e00f99602850cf3f80f4e8f400a59e3fe2555e9c05faea91822e016c429', "NOAPIKEY")
 ];
